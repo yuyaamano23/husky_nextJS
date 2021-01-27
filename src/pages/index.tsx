06ec0b689a72c5247dc7c0e53dc9b3c8fa6496f9
yuyaamano23 <nba.chako.nba@icloud.com>
2021-01-27 23:38:10 +0900

    huskycommit確認2338

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  // const Hello = 'unko'
-  return <div>Hello, World!</div>
+  return <div>Hello</div>
 }
 
 export default Home
